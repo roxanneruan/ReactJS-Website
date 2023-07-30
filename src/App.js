8a8b4ba62c1a582a6fc3a264d298a6727617e957
roxanneruan <roxanneruan@gmail.com>
2023-07-29 22:02:54 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+// We import the BrowserRouter implementation from react Dom ; this uses
+// the HTML5 API to update the URL
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path = "/" element={<Home />} />
+          <Route path = "/projects" element={<Projects />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
