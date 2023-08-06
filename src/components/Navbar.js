fdc3be355abfb639c992ba350c34497b45af1763
roxanneruan <roxanneruan@gmail.com>
2023-08-05 22:43:29 -0700

    Add Navbar.js button logo
    - imported ReorderIcon
    - adding this under our button component, if the website gets to a smaller size, a button appears instead of the full size text in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="toggleButton">
-        <button> </button> 
+        <button> 
+        <ReorderIcon / >
+        </button> 
       </div>
       <div className="links">
         <Link to="/"> Home</Link>
