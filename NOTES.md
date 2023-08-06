6d8dad5e2ffca38e2674d2b484e8005fb29a95a0
roxanneruan <roxanneruan@gmail.com>
2023-08-05 23:15:48 -0700

    Update NOTES.md and Navbar.js
    - created NOTES.md for documentation and progress tracking
    - mobile web page transitions to a logo that has a state depending on if user clicks it open or closed.

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+# REACT Portfolio Project Notes
+
+This personal portfolio website was made following this [tutorial by PedroTech](https://www.youtube.com/watch?v=x7mwVn2z3Sk&ab_channel=PedroTech). 
+
+This is my first personal project involving React, Javascript, HTML, and CSS. My intentions with following this tutorial was to gain basic knowledge and hands-on practice with React, and familiarize myself with web development. This md acts as my own "lecture notes" and a document to keep track of my progress.
+
+### Sat, Aug. 5, 2023
+* Created this md.
+* Imported useState from React to keep track of ReorderIcon state in the responsive mobile form. expandNavbar initially set to false, and then is changed states based on the onClick function which switches its state. 
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,21 @@
 // Components differ from pages ; used across multiple pages.
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
+    // const boolean to check if navbar button is clicked open or not.
+    // onClick function sets boolean to opposite of previous state.
+    const [expandNavbar, setExpandNavbar] = useState(false);
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
-        <button> 
+        <button 
+            onClick={() => {
+                setExpandNavbar((prev) => !prev);
+            }}
+        > 
         <ReorderIcon / >
         </button> 
       </div>
@@ -17,7 +24,7 @@ function Navbar() {
         <Link to="/Projects"> Projects</Link>
       </div>
     </div>  
+    // ?
   )
 }
-
 export default Navbar
