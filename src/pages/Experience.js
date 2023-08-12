53fb445df6a2fe4509f0672209846938bc995ef7
roxanneruan <roxanneruan@gmail.com>
2023-08-11 23:01:40 -0700

    Update Experience.js order

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -6,47 +6,17 @@ import {
 import 'react-vertical-timeline-component/style.min.css'
 import SchoolIcon from '@material-ui/icons/School'
 import GroupIcon from '@material-ui/icons/GroupTwoTone'
-import '../styles/Experience.css'
 
 function Experience() {
   return (
     <div className="experience">
         <VerticalTimeline lineColor="#3e497a">
-            <VerticalTimelineElement className="vertical-timeline-element--education"
-            date="Aug. 2017 - Jun. 2021"
-            iconStyle={{background: "#3e497a", color: "#fff"}} 
-            icon={<SchoolIcon />}
-            >
-                <h3 className="vertical-timeline-element-title"> 
-                    KIPP King Collegiate HS, San Lorenzo, CA 
-                </h3>
-
-                <p> High School Diploma </p>
-            </VerticalTimelineElement>
-
-            <VerticalTimelineElement className="vertical-timeline-element--education"
-            date="Sept. 2021 - Present"
-            iconStyle={{background: "#3e497a", color: "#fff"}} 
-            icon={<SchoolIcon />}
-            >
-                <h3 className="vertical-timeline-element-title"> 
-                    University of California - Davis, Davis, CA 
-                </h3>
-
-                <h4 className="vertical-timeline-element-subtitle">
-                    Bachelor's Degree
-                </h4>
-
-                <p> Computer Science </p>
-
-                <h5 className="vertical-timeline-element-info"> GPA: 3.65</h5>
-            </VerticalTimelineElement>
 
             <VerticalTimelineElement className="vertical-timeline-element--extracurricular"
-            date="Apr. 2023 - May 2023"
-            iconStyle={{background: "#3e497a", color: "#fff"}} 
-            icon={<GroupIcon />}
-            >
+                date="Apr. 2023 - May 2023"
+                iconStyle={{background: "#3e497a", color: "#fff"}} 
+                icon={<GroupIcon />}
+                >
                 <h3 className= "vertical-timeline-element-title"> 
                     UC Davis Design Interactive (UI/UX Club)
                 </h3>
@@ -66,6 +36,35 @@ function Experience() {
 
             </VerticalTimelineElement>
 
+            <VerticalTimelineElement className="vertical-timeline-element--education"
+                date="Sept. 2021 - Present"
+                iconStyle={{background: "#3e497a", color: "#fff"}} 
+                icon={<SchoolIcon />}
+                >
+                <h3 className="vertical-timeline-element-title"> 
+                    University of California - Davis, Davis, CA 
+                </h3>
+
+                <h4 className="vertical-timeline-element-subtitle">
+                    Bachelor's Degree
+                </h4>
+
+                <p> Computer Science </p>
+
+                <h5 className="vertical-timeline-element-info"> GPA: 3.65</h5>
+            </VerticalTimelineElement>
+
+            <VerticalTimelineElement className="vertical-timeline-element--education"
+            date="Aug. 2017 - Jun. 2021"
+            iconStyle={{background: "#3e497a", color: "#fff"}} 
+            icon={<SchoolIcon />}
+            >
+                <h3 className="vertical-timeline-element-title"> 
+                    KIPP King Collegiate HS, San Lorenzo, CA 
+                </h3>
+
+                <p> High School Diploma </p>
+            </VerticalTimelineElement>
         </VerticalTimeline>
       
     </div>
